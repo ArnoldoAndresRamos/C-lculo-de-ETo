24840b032f938d72876d65d953c390df5244def8
Arnoldo Andrés Ramos Acevedo <43187167+ArnoldoAndresRamos@users.noreply.github.com>
2019-11-26 21:03:59 -0300

    Add files via upload

---
+++ ETo.php
@@ -0,0 +1,32 @@
+<?php
+# Calculo de la evapotranpiracion de referncia
+# usando metodo Ecuacion de Penman-Monteith 
+
+
+# Parametros usados en el calculo  
+
+$Tmax=;   # 1.- Temperatura Maxima 		-> Tmax °C
+$Tmin=;   # 2.- Temaperatura Minima 		-> Tmin °C
+$Altitud=; # 3.- Altitud			  		-> Altitud Metros					
+$u2=;		 # 4.- Velocidad del Viento		-> u2 metros/s-1
+$HRmax=;   # 5.- humedad relativa Maxima 	-> HRmax
+$HRmin=;	 # 6.- humedad relativa Maxima 	-> HRmin
+
+
+# Temperatura media Tmedia °C
+$Tmedia = ($Tmax+$Tmin)/2;
+
+
+# Δ (Pen curva de presión de vapor) en kPa°C-1
+/*			(4098 [0.6108 e([17,27*Tmedia]+[Tmedia+237,3])])  
+ecuación	________________________________________________
+						(Tmedia+237,3)^2                       */
+
+$PenCurvaDePresionDeVapor =(4098*(0.6108*exp((17.27*$Tmedia)+($Tmedia)+237.3)))/(($Tmedia+237.3)^2);
+echo $PenCurvaDePresionDeVapor;  
+
+
+#
+
+
+?>
