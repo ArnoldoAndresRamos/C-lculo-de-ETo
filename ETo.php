fc7c6d2ebf3b7178a58717a0f8d27a77f164d945
Arnoldo Andrés Ramos Acevedo <43187167+ArnoldoAndresRamos@users.noreply.github.com>
2019-11-26 22:06:26 -0300

    Update ETo.php

--- ETo.php
+++ ETo.php
@@ -5,12 +5,12 @@
 
 # Parametros usados en el calculo  
 
-$Tmax=35.8;   # 1.- Temperatura Maxima 		-> Tmax °C
-$Tmin=6.4;   # 2.- Temaperatura Minima 		-> Tmin °C
-$Altitud=; # 3.- Altitud			  		-> Altitud Metros					
-$u2=;		 # 4.- Velocidad del Viento		-> u2 metros/s-1
-$HRmax=;   # 5.- humedad relativa Maxima 	-> HRmax
-$HRmin=;	 # 6.- humedad relativa Maxima 	-> HRmin
+$Tmax=35.8;  // 1.- Temperatura Maxima 		-> Tmax °C
+$Tmin=6.4;   // 2.- Temaperatura Minima 	-> Tmin °C
+$Altitud=0;  // 3.- Altitud			-> Altitud Metros					
+$u2=0;	     // 4.- Velocidad del Viento	-> u2 metros/s-1
+$HRmax=0;    // 5.- humedad relativa Maxima 	-> HRmax
+$HRmin=0;    // 6.- humedad relativa Maxima 	-> HRmin
 
 
 # Temperatura media Tmedia °C
@@ -18,12 +18,14 @@ $Tmedia = ($Tmax+$Tmin)/2;
 
 
 # Δ (Pen curva de presión de vapor) en kPa°C-1
-/*			(4098 [0.6108 e([17,27*Tmedia]+[Tmedia+237,3])])  
+/*		(4098 [0.6108 e([17,27*Tmedia]+[Tmedia+237,3])])  
 ecuación	________________________________________________
-						(Tmedia+237,3)^2                       */
+				(Tmedia+237,3)^2                       */
 
-$PenCurvaDePresionDeVapor =(4098*(0.6108*exp((17.27*$Tmedia)+($Tmedia)+237.3)))/(($Tmedia+237.3)^2);
-echo $PenCurvaDePresionDeVapor;  
+$PenCurvaDePresionDeVapor =4098 (0.6108 * exp(  (17.27*$Tmedia)/($Tmedia+237.3) )   /   (($Tmedia+237.3)^2);
+$PenCurvaDePresionDeVapor2 =((4098(0.6108*EXP((17.27*$Tmedia)/($Tmedia+237.3))))/(($Tmedia+237.3)^2));
+echo $PenCurvaDePresionDeVapor."<br>";
+echo $PenCurvaDePresionDeVapor2."<br>";
 
 
 #
