99245fc2ad5883a1b3ff98546003ff33719449b8
Arnoldo Andrés Ramos Acevedo <43187167+ArnoldoAndresRamos@users.noreply.github.com>
2019-11-29 16:38:28 -0300

    Update ETo.php

--- ETo.php
+++ ETo.php
@@ -151,5 +151,5 @@ function ETo($Tmax, $Tmin, $HRmax,  $HRmin, $Latitud, $Altitud, $Dia_Juliano, $u
 
 }
 
-ETo();
+ETo($Tmax, $Tmin, $HRmax,  $HRmin, $Latitud, $Altitud, $Dia_Juliano, $u2, $n );
 ?>
